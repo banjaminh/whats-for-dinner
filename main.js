f7c016fb25ff6b1b0852e3eff72a10820d5e3d2d
banjaminh <128344939+banjaminh@users.noreply.github.com>
2023-05-28 09:38:10 -0500

    Update main.js
    updated to current. Could not push from machine

--- main.js
+++ main.js
@@ -1,66 +1,124 @@
-var Side = [
-    'Miso',
-    'Coleslaw',
-    'Garden Salad',
-    'Crispy Potatoes',
-]
-
-var Main = [
-    'Spaghetti and Meatballs',
-    'Pineapple Chicken',
-    'Shakshuka',
-    'Thai Yellow Curry',
-    'Bibimbap',
-]
-
-var Dessert = [
-    'Apple Pie',
-    'Lemon Meringue Pie',
-    'Black Forest Cake',
-    'Banana Bread',
-    'Peach Cobbler',
-]
-var newVar;
+var mealBook = {
+    Side:  [
+        'Miso Glazed Carrots',
+        'Coleslaw',
+        'Garden Salad',
+        'Crispy Potatoes',
+        'Sweet Potato Tots',
+        'Coconut Rice',
+        'Caeser Salad',
+        'Shrimp Summer Rolls',
+        'Garlic Butter Mushrooms',
+        'Hush Puppies',
+    ],
+    
+    Main : [
+        'Spaghetti and Meatballs',
+        'Pineapple Chicken',
+        'Shakshuka',
+        'Thai Yellow Curry',
+        'Bibimbap',
+        'Chicken Parmesean',
+        'Butternut Squash Soup',
+        'BBQ Chicken Burgers',
+        'Ramen',
+        'Empanadas',
+        'Chicken Fried Rice',
+        'Sheet Pan Fajitas',
+        'Margarita Pizza',
+    ],
+    
+    Dessert: [
+        'Apple Pie',
+        'Lemon Meringue Pie',
+        'Black Forest Cake',
+        'Banana Bread',
+        'Peach Cobbler',
+        'Cheesecake',
+        'Funfetti Cake',
+        'Baklava',
+        'Flan',
+        'Macarons',
+        'Macaroons',
+        'Chocolate Cupcakes',
+        'Pavlova',
+        'Pumpkin Pie',
+        'Key Lime Pie',
+        'Tart Tatin',
+        'Croissants',
+        'Eclairs',
+    ]
+}
+// Variables with query selectors
 var selectedValue = document.getElementById('meal');
 var imageBox = document.getElementById('mealbox');
-console.log(imageBox);
+var recipeBox = document.querySelector('.Recipe')
+var cookImage = document.getElementById('cookpot');
+var currentRecipeType;
+var currentMealIndex;
+var deleteButton = document.querySelector('.remove');
+
+//Event listeners for button clicks/submits
+deleteButton.addEventListener('click', function(event){
+   deleteRecipe(event);})
 
 
-console.log(imageBox,"after");
 selectedValue.addEventListener('submit', function(event){
     printSelection(event);});
 
+//Event handlers    
 function printSelection(event){
     event.preventDefault();
     if(document.querySelector('input[name="Meals"]:checked')){
         var selectedOption = document.querySelector('input[name="Meals"]:checked').value;
-        var mealArray = chooseArray(selectedOption);
-        console.log(mealArray);
-        var randChoice = Math.floor(Math.random() * mealArray.length);
-        imageBox.innerHTML = 
+        currentRecipeType = chooseArray(selectedOption);
+        currentMealIndex = Math.floor(Math.random() * mealBook[currentRecipeType].length);
+        hide(cookImage);
+        show(deleteButton);
+        recipeBox.innerHTML = 
         `<article class="mealchoice">
             <em>You should make:</em>
-            <h2>${mealArray[randChoice]} !</h2>
-            <button class="remove" id="reset">Reset</button>
+            <h2>${mealBook[currentRecipeType][currentMealIndex]} !</h2>
         </article>`;
-    console.log(selectedOption);
+        show(recipeBox);
+    
     }
-else{
-    console.log("No value selected");
-}
 };
 
+function deleteRecipe(event){
+    console.log("TEST");
+    event.preventDefault();
+    mealBook[currentRecipeType].splice([currentMealIndex],1);
+    if(mealBook[currentRecipeType].length === 0){
+        recipeBox.innerHTML = `<h2>No more meals to remove!</h2>`
+        return;
+    }
+    printSelection((event));
+}
+
+//Functionality
+function clearButton(){
+    hide(recipeBox);
+    show(cookImage);
+}
+
 function chooseArray(selectedMeal){
-    
-    if(selectedMeal=== 'Side'){
-        return Side;
+    if(selectedMeal === 'Side'){
+        return 'Side';
     }
     else if(selectedMeal === 'Dessert'){
-        return Dessert;
+        return 'Dessert';
     }
     else{
-        return Main
+        return 'Main';
     }
 }
 
-    
+function hide(element){
+    element.classList.add('hidden');
+}
+
+function show(element){
+    element.classList.remove('hidden');
+}
+       
